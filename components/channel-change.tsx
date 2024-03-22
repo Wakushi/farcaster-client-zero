963c4b09e0c64468c70bba393d458e35c4e379ad
makushi <maxime.eliazord@sequodia.com>
2024-03-22 21:39:03 +0100

    init

---
+++ components/channel-change.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import * as React from "react"
+import { Newspaper } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import channels from "@/lib/channels.json"
+import type { Channel } from "@/lib/types/channel"
+
+export function ChannelSwitch({ setChannel }: any) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Newspaper className="absolute h-[1.2rem] w-[1.2rem] text-current transition-all" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        {channels.map((channel: Channel) => (
+          <DropdownMenuItem
+            key={channel.url}
+            onClick={() => setChannel(channel.url)}
+          >
+            {channel.name}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
